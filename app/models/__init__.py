c1747b9697d621f7bb82cec6f89eed4e951e8b31
Taylor McClerin <mcclerinette@gmail.com>
2023-06-03 09:18:21 -0700

    still working on models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,8 +3,8 @@ from .user import User
 from .notes import Note
 from .notebooks import Notebook
 from .tags import Tag
-
 from .note_tags import NoteTag
+from .share_privileges import share_privileges
 
 
 from .db import environment, SCHEMA

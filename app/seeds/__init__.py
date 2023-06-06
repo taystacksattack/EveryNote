877957f31c6ad086767617e1aedaf7b3876c67db
Taylor McClerin <mcclerinette@gmail.com>
2023-06-06 09:22:06 -0700

    delete almost done

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -22,7 +22,7 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        # undo_notetags()
+        undo_note_tags()
         undo_tags()
         undo_tasks()
         undo_notes()
@@ -33,7 +33,7 @@ def seed():
     seed_notes()
     seed_tasks()
     seed_tags()
-    # seed_notetags()
+    seed_note_tags()
     # seed_note_tags
     # seed_sharepriviges
 

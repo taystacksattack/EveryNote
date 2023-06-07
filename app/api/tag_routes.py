d976fdd34243d8968e6787b64c98213bf022a2db
Roberto_peregrina_Jr <robertoperegrina1996@gmail.com>
2023-06-07 11:17:21 -0700

    done

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -1,17 +1,12 @@
-from flask import Blueprint # jsonify, request
-from flask_login import login_required #current_user
-<<<<<<< HEAD
-from app.models import Tag, note_tag
-#from ..models.db import db
-#from ..forms.note_form import NoteForm
-=======
+from flask import Blueprint  # jsonify, request
+from flask_login import login_required  # current_user
 from app.models import Tag
 from ..models.db import db
-#from ..forms.tag_form import TagForm
->>>>>>> tuesday-josh
+# from ..forms.tag_form import TagForm
 
 
 tag_routes = Blueprint('tags', __name__)
+
 
 def pull_notetags(self):
     return {
@@ -20,20 +15,19 @@ def pull_notetags(self):
         'tag_id': self.tag_id
     }
 
-<<<<<<< HEAD
-=======
+
 @tag_routes.route('/<int:tagId>', methods=["DELETE"]
-# @tag_routes.route('/<int:tagId>', methods=["DELETE"]
-)
+                  # @tag_routes.route('/<int:tagId>', methods=["DELETE"]
+                  )
 @login_required
 def delete_tag(tagId):
     print("at delete route")
     to_delete = Tag.query.get(tagId)
     db.session.delete(to_delete)
     db.session.commit()
-    return { "message": f'Tag {tagId} successfully deleted'}
+    return {"message": f'Tag {tagId} successfully deleted'}
 
->>>>>>> tuesday-josh
+
 @tag_routes.route('/')
 @login_required
 def get_tags():
@@ -44,8 +38,7 @@ def get_tags():
     tags = Tag.query.all()
     return {'tags': [tag.to_dict() for tag in tags]}
 
-<<<<<<< HEAD
-=======
+
 @tag_routes.route('/', methods=["POST"])
 @login_required
 def create_tag():
@@ -68,17 +61,14 @@ def create_tag():
 
 
         """
-    test_tag = Tag( name="test_tag")
+    test_tag = Tag(name="test_tag")
     db.session.add(test_tag)
     db.session.commit()
     return test_tag.to_dict()
 
 
-
-
->>>>>>> tuesday-josh
-#also get notetags
-#does this work? query Tag// notes relationship
+# also get notetags
+# does this work? query Tag// notes relationship
 @tag_routes.route('/notetags/')
 @login_required
 def get_notetags():
@@ -86,55 +76,11 @@ def get_notetags():
     Query for all note_tags and returns them in a list of note_tag tuples, by (note_id, tag_id)
     """
 
-<<<<<<< HEAD
-    print("\n\n\n\n500 here?")
-    tags = Tag.query.all()
-
-    return { 'notetags': [tag.notes_of_tag() for tag in tags]
-    }
-
-    # print("what about this", dir(note_tag))
-    # print("\n\n\nget children????", note_tag.get_children())
-    # print("\n\n\n????", note_tag.table_valued())
-    # print("type of table_valued...?", type(note_tag.table_valued()))
-    # print("colu...?", note_tag.foreign_keys)
-    # print("metadata...?", type(note_tag.metadata))
-
-    # # print("\n\n\ntags", tags)
-    # print("pre-note-tag")
-    # print("\n\n\n\nwhat does this do", dir(note_tag))
-
-    for tag in tags:
-        print("does this work?", tag)
-
-    tempNoteTags = []
-
-    # for tag in tags:
-    #     testtag = tag.to_dict_test()
-    #     print("current tag:", testtag)
-
-    return {"tempnote": tempNoteTags}
-
-"""
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'body': self.body,
-            'trash': self.trash,
-            'ownerId': self.ownerId,
-            'notebookId': self.notebookId,
-            'created_at': self.created_at,
-            'updated_at': self.updated_at,
-            # 'tags': self.tags
-        } """
-=======
     print("\n\n\nAT NOTETAGS ROUTE")
 
     tags = Tag.query.all()
-    res = { "note_to_tags": {},
-           "tag_to_notes": {} }
-
+    res = {"note_to_tags": {},
+           "tag_to_notes": {}}
 
     tag_to_notes = res["tag_to_notes"]
     note_to_tags = res["note_to_tags"]
@@ -157,4 +103,3 @@ def get_notetags():
 
     # print("\n\n\n\does THIS work?", res)
     return res
->>>>>>> tuesday-josh

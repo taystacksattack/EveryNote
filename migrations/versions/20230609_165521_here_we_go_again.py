b00d5f78eb7a838c2134bcc44bd3e6baea2f8689
Taylor McClerin <mcclerinette@gmail.com>
2023-06-09 17:15:30 -0700

    ready for pre-grade again

--- migrations/versions/20230609_165521_here_we_go_again.py
+++ migrations/versions/20230609_165521_here_we_go_again.py
@@ -7,6 +7,9 @@ Create Date: 2023-06-09 16:55:21.032692
 """
 from alembic import op
 import sqlalchemy as sa
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.

f1f2d843076873cb8d347e6c9c659c01d2492a51
Carmen Lee <carmenlee2278@gmail.com>
2023-07-11 09:29:06 -0700

    got small icons going

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -5,7 +5,7 @@ import ProfileButton from './ProfileButton';
 import LandingPage from '../LandingPage';
 import './Navigation.css';
 
-function NavBar({ isLoaded }){
+function NavBar({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	// console.log(sessionUser)
@@ -18,20 +18,33 @@ function NavBar({ isLoaded }){
 						<ProfileButton user={sessionUser} />
 					</li>
 				)}
-				
+
 				<li className='li-links'>
-					<NavLink className='just-links'exact to="/notes">Notes</NavLink>
+					<NavLink className='just-links' exact to="/notes">
+						<span className="material-symbols-outlined">description
+						</span>
+						<span id='navlink-names'>Notes</span>
+					</NavLink>
 				</li>
 				<li className='li-links'>
-					<NavLink className='just-links'exact to="/tasks">Tasks</NavLink>
+					<NavLink className='just-links' exact to="/tasks">
+						<span className="material-symbols-outlined">task_alt</span>
+						<span id='navlink-names'>Tasks</span>
+					</NavLink>
 				</li>
 				<li className='li-links'>
-					<NavLink className='just-links'exact to="/notebooks">Notebooks</NavLink>
+					<NavLink className='just-links' exact to="/notebooks">
+						<span className="material-symbols-outlined">auto_stories</span>
+						<span id='navlink-names'>Notebooks</span>
+					</NavLink>
 				</li>
 				<li className='li-links'>
-					<NavLink className='just-links'exact to="/tags">Tags</NavLink>
+					<NavLink className='just-links' exact to="/tags">
+						<span className="material-symbols-outlined">sell</span>
+						<span id='navlink-names'>Tags</span>
+					</NavLink>
 				</li>
-				
+
 				{/* <LandingPage isLoaded = {isLoaded}/> */}
 				{/* <li>
 					<NavLink exact to="/">Trash</NavLink>
@@ -45,28 +58,28 @@ function NavBar({ isLoaded }){
 					<h3>Developer Links</h3>
 					<div id="links-wrapper">
 						<p>Carmen Lee</p>
-						<div>
+						<div className='dev-links'>
 							<a href="https://www.linkedin.com/in/carmen-lee-52061690/"><i class="fa-brands fa-linkedin"></i></a>
 							<a href="https://github.com/lee2278"><i class="fa-brands fa-github"></i></a>
 						</div>
 					</div>
 					<div id="links-wrapper">
 						<p>Joshua Lee</p>
-						<div>
+						<div className='dev-links'>
 							<a href="https://linkedin.com/in/jaewon-lee-9573a0142"><i class="fa-brands fa-linkedin"></i></a>
 							<a href="https://github.com/j1jlee/"><i class="fa-brands fa-github"></i></a>
 						</div>
 					</div>
 					<div id="links-wrapper">
 						<p>Taylor McClerin</p>
-						<div>
+						<div className='dev-links'>
 							<a href="https://www.linkedin.com/in/taylor-mcclerin-059586177/"><i class="fa-brands fa-linkedin"></i></a>
 							<a href="https://github.com/taystacksattack"><i class="fa-brands fa-github"></i></a>
 						</div>
 					</div>
 					<div id="links-wrapper">
 						<p>Roberto Peregrina Jr</p>
-						<div>
+						<div className='dev-links'>
 							<a href="https://www.linkedin.com/in/roberto-peregrina/"><i class="fa-brands fa-linkedin"></i></a>
 							<a href="https://github.com/RobertoPeregrinaJr96"><i class="fa-brands fa-github"></i></a>
 						</div>

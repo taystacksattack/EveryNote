f310122d94646800a68992952da7ae4e48d75d9e
j1jlee <enoneddih1989@gmail.com>
2023-07-17 19:19:39 -0400

    buttons replaced with icons/titles

--- react-app/src/components/TagsPage/TagDeleteModal.js
+++ react-app/src/components/TagsPage/TagDeleteModal.js
@@ -1,63 +1,62 @@
 import React, { useState } from "react";
-import { login } from "../../store/session";
+// import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import {Link} from "react-router-dom";
+import { useModal, closeModal } from "../../context/Modal";
 
-function TagDeleteModal() {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const { closeModal } = useModal();
+import { deleteTagThunk } from "../../store/tags";
+// import { renderSwitch, setRenderSwitch } from "../TagsPage";
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    } else {
-        closeModal()
-    }
-  };
 
-  const handleDemoUser = async (e) => {
-    await dispatch(login('demo@aa.io', 'password'))
-    closeModal()
-    return
+
+
+function TagDeleteModal({tagId, tagName, renderSwitch, setRenderSwitch}) {
+
+  async function deleteTest(tagId) {
+    return dispatch(deleteTagThunk(tagId))
+    .then(() => {
+        let temp = renderSwitch;
+        setRenderSwitch(!temp);
+
+        closeModal();
+    })
+    .catch(async (res) => {
+    })
 
   }
 
+  const dispatch = useDispatch();
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [errors, setErrors] = useState([]);
+  const { closeModal } = useModal();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const data = await dispatch(login(email, password));
+  //   if (data) {
+  //     setErrors(data);
+  //   } else {
+  //       closeModal()
+  //   }
+  // };
+
+  // const handleDemoUser = async (e) => {
+  //   await dispatch(login('demo@aa.io', 'password'))
+  //   closeModal()
+  //   return
+
+  // }
+
   return (
     <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-        <Link id='demo-user-link' to='/' onClick={handleDemoUser}>Demo User</Link>
+      <h1>Delete tag?</h1>
+      <div>Are you sure you want to delete the {tagName} tag? This tag will be removed from all notes.</div>
+
+      <span className="tag-node-delete-button">
+          {<button onClick={() => {deleteTest(tagId)}}>Delete</button>}
+
+          {<button onClick={() => closeModal}>Cancel</button>}
+      </span>
     </>
   );
 }
--- react-app/src/components/TagsPage/index.js
+++ react-app/src/components/TagsPage/index.js
@@ -8,10 +8,10 @@ import { getNotesThunk } from "../../store/notes"
 import "./TagsPage.css"
 // import { title } from "../NotesPage"
 
-import OpenModalButton from "../OpenModalButton";
+// import OpenModalButton from "../OpenModalButton";
 
 import OpenModalCustomIcon from "../OpenModalButton/OpenModalCustomIcon"
-// import TagDeleteModal from "./TagDeleteModal"
+import TagDeleteModal from "./TagDeleteModal"
 import TagCreateRenameModal from "./TagCreateRenameModal"
 //import DeleteGroupModal from "../DeleteGroupModal";
 // import CurrentNotes from "../NotesPage"
@@ -370,11 +370,15 @@ const TagsPage = () => {
         <div className="tag-page-wrapper">
 
         <div className="tag-list-top">
-            <h1> Tags Page!</h1>
+            <h1>
+            <span className="material-symbols-outlined">sell</span>
+            &nbsp;
+            Tags</h1>
             <br></br>
+{/*
             <p>Apply tags to better organize your notes!</p>
             <br></br>
-            <p id="tag-list-top-sentence">(The following <span id='tag-list-top-sentence-tags'>tags</span> are applied to the following <span id="tag-list-top-sentence-notes">notes</span>)</p>
+            <p id="tag-list-top-sentence">(The following <span id='tag-list-top-sentence-tags'>tags</span> are applied to the following <span id="tag-list-top-sentence-notes">notes</span>)</p> */}
             {/* <div className="tag-notetest-node">
                 {noteTest(1)}
             </div>
@@ -388,15 +392,45 @@ const TagsPage = () => {
             </div> */}
 
             <div className="tag-list-top-create-and-sort-buttons">
-                 <span id="tag-list-top-create-button">
+                 {/* <span id="tag-list-top-create-button">
                 <OpenModalButton
                     buttonText="Create New Tag"
                     modalComponent={<TagCreateRenameModal createOrRename="Create New" />}
-                />
+                    />
+                </span> */}
+{/*
+                <span className="material-symbols-outlined tag-list-top-create-icon" title="Create New Tag">library_add</span> */}
+
+                <span className="tag-list-hover-pointer">
+                <OpenModalCustomIcon
+                    modalComponent={<TagCreateRenameModal createOrRename="Create New" />}
+                    materialSymbolIconName="library_add"
+                    iconTitle="Create New Tag"
+                    />
+
                 </span>
+
+                {/* 07-17
+                OpenModalCustomIcon,
+                    modalComponent,
+                    materialSymbolIconName
+                    iconTitle/hovertext
+                    onButtonClick
+                     */}
+
+{/*
                 <span id="tag-list-top-sort-button">
                     <button onClick={toggleSort}>Toggle Sort: A-Z or #notes</button>
+                </span> */}
+
+                <span
+                className="material-symbols-outlined tag-list-hover-pointer"
+                title="Sort: A-Z or #Notes"
+                onClick={toggleSort}>
+                    sort
                 </span>
+
+
             </div>
 
         </div>
@@ -434,6 +468,31 @@ const TagsPage = () => {
                         <span className="tag-node-name-and-num-num">
                             ({tag.id && numNotesByOwner(tag.id)})
                         </span>
+
+                        {/* add 07/17 */}
+                        <span className="tag-list-rename-delete">
+
+                        <span className="tag-list-hover-pointer">
+                        <OpenModalCustomIcon
+                            modalComponent={<TagCreateRenameModal createOrRename="Rename" tag={tag} />}
+                            materialSymbolIconName="edit_square"
+                            iconTitle="Rename Tag"
+                            />
+                        </span>
+
+                        <span className="tag-list-hover-pointer">
+                        <OpenModalCustomIcon
+                            modalComponent={<TagDeleteModal
+                                tagId={tag.id}
+                                tagName={tag.name}
+                                renderSwitch={renderSwitch}
+                                setRenderSwitch={setRenderSwitch} />}
+                            materialSymbolIconName="delete"
+                            iconTitle="Delete Tag"
+                            />
+                        </span>
+
+                        </span> {/* end of two buttons */}
                     </div>
 
                     <div>
@@ -441,20 +500,17 @@ const TagsPage = () => {
                     </div>
 
                     <div className="tag-node-main-buttons">
-                    <span className="tag-node-rename-button">
+
+
+                    {/* <span className="tag-node-rename-button">
                     <OpenModalButton
                         buttonText="Rename Tag"
                         modalComponent={<TagCreateRenameModal createOrRename="Rename" tag={tag} />}
                     />
-                    </span>
+                    </span> */}
 
-                    {/* 07-17
-                    OpenModalCustomIcon,
-                        modalComponent,
-                        materialSymbolIconName
-                        iconTitle/hovertext
-                        onButtonClick
-                         */}
+
+
                     {/* <span className="tag-node-rename-button">
                     <OpenModalCustomIcon
                         // buttonText="Rename Tag"
@@ -465,9 +521,12 @@ const TagsPage = () => {
 
 
 
-                    <span className="tag-node-delete-button">
+                    {/* <span className="tag-node-delete-button">
                     {<button onClick={() => {deleteTest(tag.id)}}>Delete this Tag</button>}
-                    </span>
+                    </span> */}
+
+
+
 
                     {/* <span>
                         <button onClick={() => {removeTagFromAll(tag.id)}}>Remove Tag from ALL notes</button>
@@ -488,4 +547,5 @@ const TagsPage = () => {
     )
 }
 
-export default TagsPage
+export default TagsPage;
+// export
